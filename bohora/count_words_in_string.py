d1c77dd54c59ea70ae7c990763cc2d7fe1d17f2f
Som Bohora <som-bohora@ouhsc.edu>
2018-05-31 13:26:47 -0500

    add count number of words func

---
+++ bohora/count_words_in_string.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 31 11:23:08 2018
+
+@author: SBOHORA
+"""
+import string
+x = "This is a string. This is a a word. hora?"
+
+def count_words_in_string(x, remove_punctuation = False):
+    """
+    Count the number of words in a given string
+    :param x: x is an input string (seprated by multiple spaces)
+    :param remove_punctuation: remove_punctuation is a boolean variable whether to remove punctuation or not
+    :return: returns each unique word and its occurrences
+    Usage:
+        count_words_in_string(x)
+        count_words_in_string(x, remove_punctuation=True)
+    """
+    counts = dict()    
+    if remove_punctuation:
+        s = ''.join(i for i in x if i not in string.punctuation)
+    else:
+        s = x
+        
+    result = s.split(' ')
+    for word in result:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1 
+    return(counts)
